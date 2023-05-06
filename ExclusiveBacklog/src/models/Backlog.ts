6f245ea1e1ce64c95bf7a94471e690d11fb0c20a
Pawel <szymczyk.pawel@yahoo.co.uk>
2023-05-06 11:01:18 +0100

    trash project again

---
+++ ExclusiveBacklog/src/models/Backlog.ts
@@ -0,0 +1,11 @@
+export interface Backlog {
+  name: string;
+  price: number;
+  quantity: number;
+  category: string;
+  buyOn: string;
+  createdOn: string;
+  modifiedOn: string;
+  _id: string;
+  __v: number;
+}
