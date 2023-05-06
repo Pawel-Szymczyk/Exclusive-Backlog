9d153119ad0ce15b82bd3d6339d8b212d7b966c1
Pawel <szymczyk.pawel@yahoo.co.uk>
2023-05-06 12:38:49 +0100

    fix debugger

--- ExclusiveBacklog/App.tsx
+++ ExclusiveBacklog/App.tsx
@@ -1,11 +1,21 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View, Button} from 'react-native';
 
 export default function App() {
+  function buttonPressed() {
+    console.log('Button pressed');
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button
+        onPress={buttonPressed}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
     </View>
   );
 }
@@ -13,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
