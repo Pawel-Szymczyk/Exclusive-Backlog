99de31ec42101f182449e0fe3ab1e7b93f328b71
Pawel <szymczyk.pawel@yahoo.co.uk>
2023-05-09 21:43:58 +0100

    add react-native-paper framework (UX)

--- ExclusiveBacklog/App.tsx
+++ ExclusiveBacklog/App.tsx
@@ -4,6 +4,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {Provider} from 'react-redux';
 import store from './src/store';
+import {Provider as PaperProvider} from 'react-native-paper';
 
 // export type RootStackParamList = {
 //   HomeView: Home;
@@ -16,11 +17,13 @@ export default class App extends React.Component {
   render(): React.ReactNode {
     return (
       <Provider store={store}>
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name="HomeView" component={Home} />
-          </Stack.Navigator>
-        </NavigationContainer>
+        <PaperProvider>
+          <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen name="HomeView" component={Home} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </PaperProvider>
       </Provider>
     );
   }
