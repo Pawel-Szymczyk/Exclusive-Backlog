74014399e8284854736ac8356bbd7d808883aee5
Pawel <szymczyk.pawel@yahoo.co.uk>
2023-05-13 13:20:42 +0100

    work on form (add select button)

--- ExclusiveBacklog/src/views/NewBacklogView.tsx
+++ ExclusiveBacklog/src/views/NewBacklogView.tsx
@@ -4,8 +4,12 @@ import {Button, TextInput} from 'react-native-paper';
 import useNewBacklogViewController from '../viewcontrollers/useNewBacklogViewController';
 import {RootStackParamList} from '../../App';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import SelectDropdown from 'react-native-select-dropdown';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 type NewBacklogProps = NativeStackScreenProps<RootStackParamList, 'NewBacklog'>;
+
+const countries = ['Egypt', 'Canada', 'Australia', 'Ireland'];
 
 const NewBacklogView = ({route, navigation}: NewBacklogProps) => {
   const {formState, creatingBacklog, onChangeText, onFormSubmit} =
@@ -33,6 +37,63 @@ const NewBacklogView = ({route, navigation}: NewBacklogProps) => {
         onChangeText={(text: string) => onChangeText('name', text)}
         mode="outlined"
       />
+      <View style={styles.categoryContainer}>
+        <SelectDropdown
+          data={countries}
+          onSelect={(selectedItem, index) => {
+            console.log(selectedItem, index);
+          }}
+          defaultButtonText={'Select country'}
+          buttonTextAfterSelection={(selectedItem, index) => {
+            return selectedItem;
+          }}
+          rowTextForSelection={(item, index) => {
+            return item;
+          }}
+          buttonStyle={styles.dropdown4BtnStyle}
+          buttonTextStyle={styles.dropdown4BtnTxtStyle}
+          renderDropdownIcon={isOpened => {
+            return (
+              <FontAwesome
+                name={isOpened ? 'chevron-up' : 'chevron-down'}
+                color={'#444'}
+                size={18}
+              />
+            );
+          }}
+          dropdownIconPosition={'right'}
+          dropdownStyle={styles.dropdown4DropdownStyle}
+          rowStyle={styles.dropdown4RowStyle}
+          rowTextStyle={styles.dropdown4RowTxtStyle}
+        />
+        <Button
+          icon="camera"
+          mode="contained"
+          onPress={() => console.log('Pressed')}
+          style={{flexBasis: '50%'}}>
+          New
+        </Button>
+      </View>
+
+      {/* <TextInput
+        label="Category"
+        value={formState.category}
+        onChangeText={(text: string) => onChangeText('category', text)}
+        mode="outlined"
+      /> */}
+      {/* <TextInput
+        label="Quality"
+        keyboardType = 'numeric'
+        value={formState.quality}
+        onChangeText={(text: string) => onChangeText('quality', text)}
+        mode="outlined"
+      />
+      <TextInput
+        label="Name"
+        value={formState.name}
+        onChangeText={(text: string) => onChangeText('name', text)}
+        mode="outlined"
+      /> */}
     </View>
   );
 };
@@ -42,6 +103,24 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 5,
   },
+  categoryContainer: {
+    display: 'flex',
+    flex: 1,
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+  },
+  dropdown4BtnStyle: {
+    width: '50%',
+    height: 50,
+    backgroundColor: '#FFF',
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#444',
+  },
+  dropdown4BtnTxtStyle: {color: '#444', textAlign: 'left'},
+  dropdown4DropdownStyle: {backgroundColor: '#EFEFEF'},
+  dropdown4RowStyle: {backgroundColor: '#EFEFEF', borderBottomColor: '#C5C5C5'},
+  dropdown4RowTxtStyle: {color: '#444', textAlign: 'left'},
 });
 
 export default NewBacklogView;
